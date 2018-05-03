0b31ff96d5e7daf66cc87a0e6066be384912c520
Matthew Kennedy <matthewkennedy@outlook.com>
2018-05-02 20:24:22 -0400

    focus testing

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,34 +6,11 @@
 #include "tach_output.h"
 #include "render.h"
 
-uint16_t rpm;
-int16_t clt, iat, ecu_temp;
-float map, tps, afr, battery;
-float timing, fuel_pw;
+uint32_t rpm = 9999;
 
-void handle_data1(can_frame_t* frame)
+void handle_focus_rpm(can_frame_t* frame)
 {
-	rpm = frame->data16[0];
-	map = (frame->data16[2]) / 10.0f;
-	tps = ((short)frame->data16[3]) / 10.0f;
-
-	//Tach_SetOutput(rpm);
-}
-
-void handle_data2(can_frame_t* frame)
-{
-	clt = frame->data8[0] - 40;
-	iat = frame->data8[1] - 40;
-
-	afr = frame->data8[5] / 10.0f;
-	battery = frame->data8[6] / 10.0f;
-	ecu_temp = frame->data8[6] - 40;
-}
-
-void handle_data3(can_frame_t* frame)
-{
-	timing = frame->data16[0] / 100.0f;
-	fuel_pw = frame->data16[1] / 1000.0f;
+	rpm = (256 * (frame->data8[4] - 96) + frame->data8[5]) * 2;
 }
 
 #include "stdint.h"
@@ -47,10 +24,7 @@ uint16_t rgb(uint8_t r, uint8_t g, uint8_t b) {
 	  c |= g >> 2;
 	  c <<= 5;
 	  c |= b >> 3;
-
 	return c;
-
-	//return c;
 }
 
 int main(void)
@@ -61,20 +35,20 @@ int main(void)
 
 	//Tach_Init(8);
 
-	/*CAN_Init();
+	CAN_Init();
 
-	CAN_RegisterReceptionHandler(0x500, &handle_data1);
-	CAN_RegisterReceptionHandler(0x501, &handle_data2);
-	CAN_RegisterReceptionHandler(0x502, &handle_data3);*/
+	CAN_RegisterReceptionHandler(0x090, &handle_focus_rpm);
 
 	LCD_Init();
 
 	DMA2DRender r(framebuffer, 96, 64);
 
-	int n = 0;
+	//int n = 0;
 
 	while(1)
 	{
+		int n = rpm;
+
 		char str[5];
 		str[0] = n / 1000 + '0';
 		str[1] = (n / 100) % 10 + '0';
@@ -84,18 +58,21 @@ int main(void)
 
 		r.FillRect(0, 0, 96, 64, 0x0000);
 
-		r.DrawString(0, 0, 0, str, 0x00ffff00);
-
-		r.DrawString(0, 30, 0, str, 0x0000ffff);
+		r.DrawString(0, 0, 0, str, 0x00ff8000);
+		r.DrawString(0, 30, 0, str, 0x000080ff);
 
 		LCD_WriteFramebuffer(framebuffer);
 
+		/*can_frame_t f;
+		f.data32[0] = 0;
+		f.data32[1] = 0;
 
-		n++;
-		/*for(int i = 0; i < 1000000; i++)
-		{
-			__asm__("nop");
-		}*/
+		f.data8[5] = 0xE2;
+		f.data8[4] = 96 + 4;
+		f.id = 0x090;
+
+		CAN_send(&f);*/
+
 	}
 
 
